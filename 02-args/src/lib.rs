7369d55af2d5345df26d803ea58b5fd60169cdde
lonnen <chris.lonnen@gmail.com>
2019-05-02 14:43:01 -0700

    embrace and extend - second simplest bci
    building off the last interpreter we clone it into a new library, the worlds second simplest bytecode interpreter (wssbi). This extends the first by adding arguments for opcodes. in kazanovs example this is done by adding opcodes that can increment the pointer and read ahead one space. Sternberg's rust articles use this to illustrate the enhanced power of rust enums here. Since enum is a sum type it can carry information! This necessarily diverges from Kazanov's internal representation which relies on C enums actually being integers under the hood. There's a bit of undefined behavior here in the C++03 standard, where the exact integral type is left to the compiler. This lets you put other integrals in the programs IR and then coerce them as needed.
    
    In this commit I've set up the new instructions as noops and left the implementation undecided. I'm tempted to jump right to implementing a stack here.

---
+++ 02-args/src/lib.rs
@@ -0,0 +1,75 @@
+pub struct VM { 
+    program: Vec<Opcode>,
+    accumulate: i64
+}
+
+pub enum Opcode {
+    Inc,
+    Dec,
+    Addi,
+    Subi,
+    Done,
+}
+
+pub fn interpret(program: Vec<Opcode>) -> Option<i64> {
+    let mut vm = VM {
+        program: program,
+        accumulate: 0,
+    };
+
+    for i in vm.program {
+        vm.accumulate = match i {
+            Opcode::Inc => vm.accumulate + 1,
+            Opcode::Dec => vm.accumulate + 2,
+            Opcode::Addi => vm.accumulate,
+            Opcode::Subi => vm.accumulate,
+            Opcode::Done => break
+        }
+    }
+
+    return Some(vm.accumulate)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn inc() {
+        use Opcode::*;
+        assert_eq!(
+            interpret(vec![Inc]), // note, no `Done`
+            Some(1)
+        );
+    }
+
+    #[test]
+    fn dec() {
+        use Opcode::*;
+        assert_eq!(
+            interpret(vec![Dec, Done]),
+            Some(-1)
+        );
+    }
+
+    #[test]
+    fn long_program() {
+        use Opcode::*;
+        assert_eq!(
+            interpret(vec![Inc, Dec, Inc]),
+            Some(1)
+        );
+    }
+
+    #[test]
+    fn instructions_past_done() {
+        use Opcode::*;
+        assert_eq!(
+            interpret(vec![Inc, Dec, Inc, Done, Inc]),
+            Some(1)
+        );
+    }
+
+    #[test]
+    fn 
+}
