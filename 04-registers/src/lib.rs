3088d78cad0b5d04cd8aa813459f8fed062c8b8c
lonnen <chris.lonnen@gmail.com>
2019-06-01 19:29:09 -0700

    all the register tests
    this adds a more exhaustive suite of tests of the register machine. The tests explicitly reveal two issues with division. First, that we were not handling division by zero (the Error was there but unused. It came over from the initial copy of vm 03's source). Second that Rust's underlying 'truncation toward zero' property leaks through. This isn't necessarily a problem as our VM is not well defined. More of a curiousty that the tests spell out clearly.
    
    Sternberg points out that this VM is not doing sufficient error checking for us. Uninitialized memory is not tracked (indeed, this patch adds tests to demonstrate memory is initialized at 0), nor is out of bounds access handled well (it panics).
    
    In future work, the macro pattern from vm 03 could add bounds checking to these opcodes. Also a lot of these tests could more concisely replaced with quick check.

--- 04-registers/src/lib.rs
+++ 04-registers/src/lib.rs
@@ -33,7 +33,7 @@ pub fn interpret(program: Vec<Opcode>) -> Result<i64> {
 
     for op in vm.program {
         match op {
-            Opcode::Load(source_0, imm) => vm.registers[source_0] = imm,
+            Opcode::Load(source_0, value) => vm.registers[source_0] = value,
             Opcode::Add(source_0, source_1, destination) => {
                 vm.registers[destination] = vm.registers[source_0] + vm.registers[source_1]
             },
@@ -44,6 +44,9 @@ pub fn interpret(program: Vec<Opcode>) -> Result<i64> {
                 vm.registers[destination] = vm.registers[source_0] * vm.registers[source_1]
             },
             Opcode::Div(source_0, source_1, destination) => {
+                if vm.registers[source_1] == 0 {
+                    return Err(ProgramError::DivisionByZero);
+                }
                 vm.registers[destination] = vm.registers[source_0] / vm.registers[source_1]
             }
             Opcode::Done(source_0) => return Ok(vm.registers[source_0])
@@ -58,22 +61,245 @@ mod tests {
     use super::*;
 
     #[test]
-    fn inc() {
+    fn ops() {
         use Opcode::*;
+
+        // no Done instruction should error
+        assert!(interpret(vec![Load(1, 100)]).is_err());
+        assert!(interpret(vec![]).is_err());
+
+        // registers are zeroed at initialization
+        for reg in 0..REGISTER_COUNT {
+            assert_eq!(interpret(vec![Done(reg)]).unwrap(), 0);
+        }
+        
+        // load a value
         assert_eq!(interpret(vec![Load(0, 2), Done(0)]).unwrap(), 2);
-        assert_eq!(interpret(vec![Load(1,2), Load(2, 3), Mul(1, 2, 0), Done(0)]).unwrap(), 6);
+
+        // addition is commutative 1 of 2
         assert_eq!(
             interpret(vec![
-                Load(1, 2),
-                Load(2, 2),
-                Mul(1, 2, 0),
+                Load(0, 2),
                 Load(1, 3),
-                Mul(1, 0, 0),
-                Load(1, 4),
-                Mul(1, 0, 0),
+                Add(0, 1, 0),
                 Done(0)
             ]).unwrap(),
-            48
+            5
         );
+
+        // addition is commutative 2 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 3),
+                Load(1, 2),
+                Add(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            5
+        );
+
+        // addition should not modify memory
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Add(0, 1, 0),
+                Done(1)
+            ]).unwrap(),
+            2
+        );
+
+        // no other memory should be impacted
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Add(0, 1, 0),
+                Done(2)
+            ]).unwrap(),
+            0
+        );
+
+
+        // subtraction is not commutative 1 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 3),
+                Sub(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            -1
+        );
+
+        // subtraction is commutative 2 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 3),
+                Load(1, 2),
+                Sub(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            1
+        );
+
+        // subtraction should not modify memory
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Sub(0, 1, 0),
+                Done(1)
+            ]).unwrap(),
+            2
+        );
+
+        // no other memory should be impacted
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Sub(0, 1, 0),
+                Done(2)
+            ]).unwrap(),
+            0
+        );
+
+
+        // multiplication is commutative 1 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 3),
+                Mul(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            6
+        );
+
+        // multiplication is commutative 2 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 3),
+                Load(1, 2),
+                Mul(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            6
+        );
+
+        // addition should not modify memory
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Mul(0, 1, 0),
+                Done(1)
+            ]).unwrap(),
+            2
+        );
+
+        // no other memory should be impacted
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Mul(0, 1, 0),
+                Done(2)
+            ]).unwrap(),
+            0
+        );
+
+
+        // division is not commutative 1 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 6),
+                Load(1, 3),
+                Div(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            2
+        );
+
+        // division is commutative 2 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 3),
+                Load(1, 6),
+                Div(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            0
+        );
+
+        // division should not modify memory
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Div(0, 1, 0),
+                Done(1)
+            ]).unwrap(),
+            2
+        );
+
+        // no other memory should be impacted
+        assert_eq!(
+            interpret(vec![
+                Load(0, 2),
+                Load(1, 2),
+                Div(0, 1, 0),
+                Done(2)
+            ]).unwrap(),
+            0
+        );
+
+        // integer division rounds down 1 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 4),
+                Load(1, 3),
+                Div(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            1
+        );
+    
+        // integer division rounds down 2 of 2
+        assert_eq!(
+            interpret(vec![
+                Load(0, 3),
+                Load(1, 4),
+                Div(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            0
+        );
+
+        // divide zero by anything
+        assert_eq!(
+            interpret(vec![
+                Load(0, 0),
+                Load(1, 4),
+                Div(0, 1, 0),
+                Done(0)
+            ]).unwrap(),
+            0
+        );
+        
+        // divide by zero
+        assert!(
+            interpret(vec![
+                Load(0, 1),
+                Load(1, 0),
+                Div(0, 1, 0),
+                Done(0)
+            ]).is_err()
+        );
+
+        // access out of range
+        //assert_eq!(interpret(vec![Done(REGISTER_COUNT + 1)]).unwrap(), 0);
+        // unimplemented. this simply panics, now
     }
 }
