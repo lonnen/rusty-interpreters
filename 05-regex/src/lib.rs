4e4623c33337aef9cb819661d4767e41a846146d
lonnen <chris.lonnen@gmail.com>
2019-06-02 13:52:55 -0700

    fill out the main loop
    the main loop of the program uses recursion as an implicit machine state passing around the instruction stream, current instruction, and current symbol. This is still pretty far from a working machine because it doens't do anything other than jump around the string.

--- 05-regex/src/lib.rs
+++ 05-regex/src/lib.rs
@@ -1,26 +1,39 @@
 pub enum Opcode {
     // match a single char to an immediate argument from the string
     // and advance ip and cp, or abort
-    Char,
-    // jump to and match either left expression or the right one, or abort
-    Or,
-    // do an absolute jump to an offset in the immediate argument
-    Jump,
+    Char(char),
+    // do an absolute jump to an offset, may be negative
+    Jump(isize),
+    // try to match the left or the right, effectively performing a
+    // d-f s  for all regex options
+    Or(isize, isize),
     // stop the execution and report a successful match
     Match,
 }
 
-pub enum Result {
-    Ok,
-    Fail,
-    Error,
+pub fn vm_match_recur(
+    program: &[Opcode], mut instruction: isize, mut symbol: char) -> bool {
+    loop {
+        match program[instruction as usize] {
+            Opcode::Char(c) => {
+                instruction += 1;
+                
+            },
+            Opcode::Jump(i) => {
+                instruction += i;
+            },
+            Opcode::Or(left, right) => {
+                if vm_match_recur(program, instruction + left, symbol) {
+                    return true;
+                }
+                instruction += right;
+            },
+            Opcode::Match => return true,
+        }
+    }
 }
 
-pub fn vm_match_recur(bytecode: Vec<Opcode>, ip: u8, sp: char) -> bool {
-    return true; // stubbed
-}
-
-pub fn vm_match(bytecode: Vec<Opcode>, sample: &str) -> bool {
+pub fn vm_match(program: Vec<Opcode>, sample: &str) -> bool {
     return true; // stubbed
 }
 
